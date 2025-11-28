5f28a8ccb3931dfd9ac0cb69ed895747993003dd
Ralph Juliano <ralph.juliano@myokanagan.bc.ca>
2025-11-28 14:38:14 -0800

    Part 8

--- src/lab5/tests/AllTests.java
+++ src/lab5/tests/AllTests.java
@@ -11,7 +11,8 @@ import org.junit.platform.suite.api.Suite;
 				TestBorrowBooks.class,
 				TestNotAvailablePaperBook.class,
 				TestRemoveMemberWithBooks.class,
-				TestController.class})
+				TestController.class,
+				TestBookFactories.class})
 public class AllTests {
     // No need for test methods here; the suite will run the included classes
 }
